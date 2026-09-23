edcdf8c1ca96cb5f5f0507a483c8f6b0b5e0d15a
menolmao24 <testaccmeno@gmail.com>
2026-09-23 05:26:40 +0200

    update

--- Week 2/sketch.js
+++ Week 2/sketch.js
@@ -7,16 +7,29 @@ let Cloud2PosX;
 let Cloud2PosY;
 
 
-
-
-
 function setup() {
   createCanvas(1280,720);
+  
 }
 
 function draw() {
   background(220);
-  fill(0,165,255)
-  rect(0,0,1920,1080)
-  circle()
+  
+  //  Background stuff
+  fill(0,165,255);
+  rect(0,0,1920,1080);
+
+  // clouds
+  noStroke();
+  fill("white");
+  circle(55,100,50,50);
+  circle(100,100,50,50);
+  circle(75,65,50,50);
+  
+}
+
+
+function Clouds()
+{
+
 }
